240c87f9c69d07547dd80ef840289cf958593c35
nacchan-kun <sendono.nc17@gmail.com>
2025-05-12 18:53:07 +0300

    fix

--- cd/src/main.ts
+++ cd/src/main.ts
@@ -1,7 +1,18 @@
-import './style.css'
+// import ''
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+
+import './task-1.ts'
+import './task-2.ts'
+import './task-3.ts'
+import './task-4.ts'
+import './task-5.ts'
+import './task-6.ts'
+import './task-7.ts'
+import './task-8.ts'
+
+
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
